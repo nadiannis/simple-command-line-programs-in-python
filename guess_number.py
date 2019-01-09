b9773caf10e3a4bcd81feae2664cb695845fccee
Annisa Nadia <annisanadianeyla@gmail.com>
2019-01-09 23:39:11 +0700

    Create new program: guess the number

---
+++ guess_number.py
@@ -0,0 +1,26 @@
+### Created on 09-01-2019
+### @author: nad
+
+import random
+
+num = random.randint(1, 100)
+print('\tI\'m thinking of a number between 1 and 100')
+print('\tYou only have 8 chances to guess.')
+
+guesses_taken = 0
+while guesses_taken < 8:
+    guess = int(input('Take a guess: '))
+
+    guesses_taken += 1
+
+    if guess > num:
+        print('Too high!\n')
+    elif guess < num:
+        print('Your guess is too low.\n')
+    elif guess == num:
+        break 
+
+if guess == num:
+    print('Congratulations! You guessed my number in', guesses_taken, 'guesses!')
+else:
+    print('Sorry, you\'re wrong. The number is', num)
