e6e6fa8aaeb5209096b67e438d015f0e2513e7db
Annisa Nadia <annisanadianeyla@gmail.com>
2019-01-11 23:44:12 +0700

    Add comments to char_counter.py

--- mini_programs/char_counter.py
+++ mini_programs/char_counter.py
@@ -1,10 +1,11 @@
-### Created on 11-1-2018
+### Created on 11-1-2019
 ### @author: nad
 
 while True:
     print('\nEnter text (blank to quit): ')
     text = input()
-
+    
+    # Stop the program
     if text == '':
         break
 
@@ -15,7 +16,8 @@ while True:
             count[char] = 0
 
         count[char] = count[char] + 1
-
+    
+    # Display the result
     for k, v in count.items():
         if v == 1:
             print(k + ': ' + str(v) + ' character')
