382203accd2842667f9e16eba8dcb9e88b0425ee
Annisa Nadia <annisanadianeyla@gmail.com>
2019-01-11 23:30:16 +0700

    Create new program: character counter

---
+++ mini_programs/char_counter.py
@@ -0,0 +1,23 @@
+### Created on 11-1-2018
+### @author: nad
+
+while True:
+    print('\nEnter text (blank to quit): ')
+    text = input()
+
+    if text == '':
+        break
+
+    count = {}
+    for char in text:
+        # count.setdefault(char, 0)
+        if char not in count:
+            count[char] = 0
+
+        count[char] = count[char] + 1
+
+    for k, v in count.items():
+        if v == 1:
+            print(k + ': ' + str(v) + ' character')
+        else:
+            print(k + ': ' + str(v) + ' characters')
