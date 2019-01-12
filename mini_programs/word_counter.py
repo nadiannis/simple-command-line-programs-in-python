6264e00c1c591e4b5874a2d7ebd7bbf1a070b435
Annisa Nadia <annisanadianeyla@gmail.com>
2019-01-12 23:32:47 +0700

    Simplify & improve the code in word_counter.py

--- mini_programs/word_counter.py
+++ mini_programs/word_counter.py
@@ -1,29 +1,32 @@
 ### Created on 29-12-2017
-### @author: nad
+### @author: nad.
 
+# Count the words
+def count_words(s):
+	num_of_words = len(s.split())
+	return num_of_words
+
+# Display the number of words in the text the user typed
+def count_words_text(text):
+    print(count_words(text), 'words\n')
+
+# Display the number of words in the file
+def count_words_file(the_file):
+    file = open(the_file, 'r', encoding='utf-8')
+    text = file.read()
+    print(count_words(text), 'words\n')
+    
 while True:
-    print('[Type -stop- to stop the program.]')
-    selected = str(input('Choose: type text or from file? '))
+    selected = input('Choose: type text or from file? (blank to quit)\n')
 
-    if selected == '-stop-':
+    # Stop the program
+    if selected == '':
         break
 
     if selected == 'type text':
-        text = str(input('\nType text:\n'))
-
-        def count_words(string):
-            number_of_words = len(string.split())
-            print(str(number_of_words), 'words\n')
-
-        count_words(text)
+        input_text = input('\nType text:\n')
+        count_words_text(input_text)
 
     if selected == 'from file':
-        insert_file = str(input('\nInsert file:\n'))
-
-        def count_words_from_file(the_file):
-            file = open(the_file, 'r', encoding='utf-8')
-            text = file.read()
-            number_of_words = len(text.split())
-            print(str(number_of_words), 'words\n')
-
-        count_words_from_file(insert_file)
+        insert_file = input('\nInsert file:\n')
+        count_words_file(insert_file)
