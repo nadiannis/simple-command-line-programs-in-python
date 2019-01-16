861c1262eeaba65659c5e58a15313ebc6c0cc295
Annisa Nadia <annisanadianeyla@gmail.com>
2019-01-16 23:09:43 +0700

    Add some codes in rock_paper_scissors.py

--- mini_programs/rock_paper_scissors.py
+++ mini_programs/rock_paper_scissors.py
@@ -10,21 +10,35 @@ while True:
         break
 
     if player == computer:
+        print('\tComputer\'s choice:', computer)
+        print('\tYour choice:', player)
         print('Tie!!')
     elif player == 'Rock':
         if computer == 'Paper':
+            print('\tComputer\'s choice:', computer)
+            print('\tYour choice:', player)
             print('You lose!', computer, 'covers', player)
         else:
+            print('\tComputer\'s choice:', computer)
+            print('\tYour choice:', player)
             print('You win!', player, 'smashes', computer)
     elif player == 'Paper':
         if computer == 'Rock':
+            print('\tComputer\'s choice:', computer)
+            print('\tYour choice:', player)
             print('You win!', player, 'covers', computer)
         else:
+            print('\tComputer\'s choice:', computer)
+            print('\tYour choice:', player)
             print('You lose!', computer, 'cut', player)
     elif player == 'Scissors':
         if computer == 'Rock':
+            print('\tComputer\'s choice:', computer)
+            print('\tYour choice:', player)
             print('You lose!', computer, 'smashes', player)
         else:
+            print('\tComputer\'s choice:', computer)
+            print('\tYour choice:', player)
             print('You win!', player, 'cut', computer)
     else:
         print('Invalid play. Please check your spelling.')
