a664fe46e654d124903ba86fd79b133bb8cfd07d
Annisa Nadia <annisanadianeyla@gmail.com>
2019-04-07 00:44:07 +0700

    Add some comments

--- mini_programs/word_counter.py
+++ mini_programs/word_counter.py
@@ -3,14 +3,16 @@
 
 # Count the words
 def count_words(s):
-    num_of_words = len(s.split())
-	return num_of_words
+	num_of_words = len(s.split())
+    return num_of_words
 
+# Count the words in the file
 def count_words_file(the_file):
     file = open(the_file, 'r', encoding='utf-8')
     text = file.read()
     return count_words(text)
 
+# Display the number of words
 def display_num_of_words(n):
     print(n, 'words\n')
     
