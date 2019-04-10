eeca377be1dee5fb863c20f8afc6624832d03f35
annadineyl <annisanadianeyla@gmail.com>
2019-04-10 11:26:12 +0700

    Create new program: prime number generator

---
+++ mini_programs/prime_number.py
@@ -0,0 +1,31 @@
+### Created on 10-04-2019
+### @author: nad.
+
+num_of_prime = 0
+
+def n_factor(num):
+    num_of_factor = 0
+    for i in range(1, num+1):
+        if num % i == 0:
+            num_of_factor += 1
+    
+    return num_of_factor
+
+print('######################################')
+print('###     PRIME NUMBER GENERATOR     ###')
+print('######################################')
+print('###                                ###')
+print('###     GENERATE PRIME NUMBERS     ###')
+print('###   FROM 0 UPTO A GIVEN NUMBER   ###')
+print('###                                ###')
+print('######################################')
+maxNum = int(input('Enter the maximum number: '))
+
+print('\nPrime numbers from 0 upto ' + str(maxNum) + ': ')
+for i in range(maxNum+1):
+    if n_factor(i) == 2:
+        print(i, end='  ')
+        num_of_prime += 1
+
+print('\n\nThere are '+ str(num_of_prime) + ' prime numbers.')
+    
