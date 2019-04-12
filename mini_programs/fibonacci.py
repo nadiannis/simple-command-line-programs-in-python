227941109d8c77ed89ac0f521bfce20e373b0c8f
annadineyl <annisanadianeyla@gmail.com>
2019-04-12 23:51:47 +0700

    Loop the program until the key to stop is entered

--- mini_programs/fibonacci.py
+++ mini_programs/fibonacci.py
@@ -1,16 +1,23 @@
 ### Created on 11-04-2019
 ### @author: nad.
 
-a = 0
-b = 1
-sum = 0
+while True:
+    n = int(input('\nHow many fibonacci numbers do you want? (0 to quit)\n'))
 
-n = int(input('How many fibonacci numbers do you want?\n'))
+    if n == 0:
+        break
+    elif n < 0:
+        print('Invalid input. Enter a positive number.')
+    else:
+        a = 0
+        b = 1
+        sum = 0
 
-print('\nFibonacci sequence upto', n, ':')
-for count in range(n):
-    print(a, end='  ')
+        print('\nFibonacci sequence upto ' + str(n) + ':')
+        for count in range(n):
+            print(a, end='  ')
 
-    sum = a + b
-    a = b 
-    b = sum
+            sum = a + b
+            a = b 
+            b = sum
+        print('')
