e088beb1b982ae291e1f5debb98e5a0d485a4606
annadineyl <annisanadianeyla@gmail.com>
2019-04-15 09:51:23 +0700

    Create new program: even & odd checker

---
+++ mini_programs/even_odd_checker.py
@@ -0,0 +1,14 @@
+### Created on 15-04-2019
+### @author: nad.
+
+while True:
+    num = input('Enter a number (blank to quit): ')
+
+    if num == '':
+        break
+
+    num = int(num)
+    if num % 2 == 0:
+        print(str(num) + ' is even\n')
+    else:
+        print(str(num) + ' is odd\n')
