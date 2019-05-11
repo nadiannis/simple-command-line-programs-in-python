1e49bf63ef5c68c301dcc1c7e6d03725d907990e
annadineyl <annisanadianeyla@gmail.com>
2019-05-11 22:27:03 +0700

    Create new program: average

---
+++ mini_programs/average.py
@@ -0,0 +1,16 @@
+nums = input('Enter some numbers (separate by space): ')
+nums = nums.split(' ')
+
+# Calculate the average
+sum_of_nums = 0
+for num in nums:
+    sum_of_nums += float(num) 
+
+avg = sum_of_nums / len(nums)
+
+# Display the numbers & the average
+print('\nYou enter:', end=' ')
+for num in nums:
+    print(num, end=' ')
+
+print('\nThe average:', avg)
