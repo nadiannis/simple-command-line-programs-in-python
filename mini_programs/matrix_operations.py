cd5dfd27d1d9d7e3707b4b6958126b71c22efa05
annadineyl <annisanadianeyla@gmail.com>
2019-05-27 00:57:38 +0700

    Add subMatrix function in matrix_operations.py

--- mini_programs/matrix_operations.py
+++ mini_programs/matrix_operations.py
@@ -15,6 +15,13 @@ def addMatrix(matrix1, matrix2, row, col, result):
             r.append(matrix1[i][j] + matrix2[i][j])
         result.append(r)
 
+def subMatrix(matrix1, matrix2, row, col, result):
+    for i in range(row):
+        r = []
+        for j in range(col):
+            r.append(matrix1[i][j] - matrix2[i][j])
+        result.append(r)
+
 def displayMatrix(matrix, row, col):
     for i in range(row):
         for j in range(col):
@@ -26,24 +33,42 @@ matrix1 = []
 matrix2 = []
 result = []
 
-row1 = int(input('Enter number of rows of matrix 1: '))
+print('====================================')
+print('Select operation: + Add | - Subtract')
+print('====================================')
+op = input('Enter your choice (+, -): ')
+
+row1 = int(input('\nEnter number of rows of matrix 1: '))
 col1 = int(input('Enter number of columns of matrix 1: '))
 
 row2 = int(input('\nEnter number of rows of matrix 2: '))
 col2 = int(input('Enter number of columns of matrix 2: '))
 
-if (row1 != row2) or (col1 != col2):
-    print('\n\tMatrices can\'t be added each other.')
-    print('\tBoth matrices must be of the same size.')
-else:
-    print('\n###  Matrix 1  ###')
-    inputMatrix(matrix1, row1, col1)
+if op == '+':
+    if (row1 != row2) or (col1 != col2):
+        print('\n\tMatrices can\'t be added each other.')
+        print('\tBoth matrices must be of the same size.')
+    else:
+        print('\n###  Matrix 1  ###')
+        inputMatrix(matrix1, row1, col1)
 
-    print('\n###  Matrix 2  ###')
-    inputMatrix(matrix2, row2, col2)
+        print('\n###  Matrix 2  ###')
+        inputMatrix(matrix2, row2, col2)
     
-    # Add the matrices
-    addMatrix(matrix1, matrix2, row1, col1, result)
+        addMatrix(matrix1, matrix2, row1, col1, result)
+        print('\nThe addition of the matrices:')
+        displayMatrix(result, row1, col1)
+elif op == '-':
+    if (row1 != row2) or (col1 != col2):
+        print('\n\tMatrices can\'t be subtracted each other.')
+        print('\tBoth matrices must be of the same size.')
+    else:
+        print('\n###  Matrix 1  ###')
+        inputMatrix(matrix1, row1, col1)
 
-    print('\nThe addition of the matrices:')
-    displayMatrix(result, row1, col1)
+        print('\n###  Matrix 2  ###')
+        inputMatrix(matrix2, row2, col2)
+    
+        subMatrix(matrix1, matrix2, row1, col1, result)
+        print('\nThe subtraction of the matrices:')
+        displayMatrix(result, row1, col1)
