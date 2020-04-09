2c845510c031922ae38f93e55f3ad53322872445
greyhere <57859708+greyhere@users.noreply.github.com>
2020-04-09 14:04:15 +0000

    Efficiency improvements - efficient algorithm for generating prime numbers - improved memory efficiency while dealing with large files in word_counter.py

--- mini_programs/prime_number.py
+++ mini_programs/prime_number.py
@@ -1,21 +1,6 @@
 ### Created on 10-04-2019
+import math
 
-num_of_prime = 0
-
-def n_factor(num):
-    num_of_factor = 0
-    for i in range(1, num+1):
-        if num % i == 0:
-            num_of_factor += 1
-    
-    return num_of_factor
-
-def is_prime(num, n_prime):
-    if n_factor(num) == 2:
-        print(num, end='  ')
-        n_prime += 1
-    
-    return n_prime
 
 print('######################################')
 print('###     PRIME NUMBER GENERATOR     ###')
@@ -25,10 +10,19 @@ print('###     GENERATE PRIME NUMBERS     ###')
 print('###   FROM 0 UPTO A GIVEN NUMBER   ###')
 print('###                                ###')
 print('######################################')
+
 max_num = int(input('Enter the maximum number: '))
 
-print('\nPrime numbers from 0 upto ' + str(max_num) + ': ')
-for i in range(max_num+1):
-    num_of_prime = is_prime(i, num_of_prime)
+primes = []
+for i in range(2, max_num+1):
+    primes.append(i)
 
-print('\n\nThere are ' + str(num_of_prime) + ' prime numbers.')
+for x in range(2, int(math.sqrt(max_num)+1)):
+    if x in primes:  # remove multiples of x if x in list
+        for y in range(x*2, max_num+1, x):
+            if y in primes:
+                primes.remove(y)
+
+print('\nPrime numbers from 0 upto ' + str(max_num) + ': ')
+print (*primes, end= '  ')
+print('\n\nThere are ' + str(len(primes)) + ' prime numbers.')
